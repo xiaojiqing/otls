f696ee5f15142b364993e8e9d63d80c6736a36ab
Xiang Xie <xiexiangiscas@gmail.com>
2022-12-31 21:48:36 +0800

    aesgcm

--- prf/aesgcm.h
+++ prf/aesgcm.h
@@ -1,9 +1,12 @@
 #ifndef _AES_GCM_H_
 #define _AES_GCM_H_
 #include "emp-tool/emp-tool.h"
+#include "utils.h"
 using namespace emp;
 
 static block R = makeBlock(0xe100000000000000, 0x00);
+const string circuit_file_location = macro_xstr(EMP_CIRCUIT_PATH) + string("bristol_fashion/");
+static BristolFashion aes = BristolFashion((circuit_file_location + "aes_128.txt").c_str());
 
 inline block rsht(block x, size_t i) {
     uint64_t* data = (uint64_t*)&x;
@@ -27,4 +30,149 @@ inline block mulBlock(block x, block y) {
     return Z;
 }
 
+inline block ghash(block h, block* x, size_t m) {
+    block y = zero_block;
+    for (int i = 0; i < m; i++) {
+        y = mulBlock((y ^ x[i]), h);
+    }
+    return y;
+}
+class AES_GCM {
+   public:
+    Integer key;
+    Integer H;
+    AES_GCM(Integer _key) : key(_key) { H = computeH(); }
+
+    ~AES_GCM() {}
+
+    inline Integer computeH() {
+        Integer o;
+        Integer zero(128, 0, PUBLIC);
+        Bit* in = new Bit[256];
+        memcpy(in, key.bits.data(), 128);
+        memcpy(in + 128, zero.bits.data(), 128);
+
+        aes.compute(o.bits.data(), in);
+
+        delete[] in;
+        return o;
+    }
+
+    inline Integer inc(Integer& counter, size_t s) {
+        if (counter.size() < s) {
+            error("invalid length s!");
+        }
+        Integer msb = counter, lsb = counter;
+        msb.bits.erase(msb.bits.begin(), msb.bits.begin() + s);
+        lsb.bits.erase(lsb.bits.begin() + s, lsb.bits.end());
+        lsb = lsb + Integer(s, 1, PUBLIC);
+
+        concat(msb, &lsb, 1);
+        return msb;
+    }
+
+    inline void gctr(Integer& res, Integer& counter, size_t m) {
+        Integer tmp;
+        Bit* content = new Bit[128];
+        memcpy(content, key.bits.data(), 128);
+        for (int i = 0; i < m; i++) {
+            memcpy(content + 128, counter.bits.data(), 128);
+            aes.compute(tmp.bits.data(), content);
+
+            concat(res, &tmp, 1);
+            counter = inc(counter, 32);
+        }
+        delete[] content;
+    }
+
+    inline void enc(NetIO* io, unsigned char* ctxt, unsigned char* tag, unsigned char* iv, unsigned char* msg, size_t m, unsigned char* aInfo, size_t info_len, int party) {
+        if (sizeof(iv) != 12) {
+            error("invalid IV length!");
+        }
+        reverse(iv, iv + sizeof(iv));
+        Integer J(96, iv, PUBLIC);
+        Integer ONE = Integer(32, 1, PUBLIC);
+        concat(J, &ONE, 1);
+
+        size_t u = 128 * ((m * 8 + 128 - 1) / 128) - m * 8; // 128 * ceil(8m/128) - 8m
+        // size_t v = 128 * ((info_len * 8 + 128 - 1) / 128) - info_len * 8; // 128 *
+        // ceil(8*info_len/128) - info_len*8
+        size_t ctr_len = (m * 8 + 128 - 1) / 128;
+
+        Integer Z;
+        gctr(Z, J, 1 + ctr_len);
+
+        // Integer H = computeH();
+        H.bits.insert(H.bits.end(), Z.bits.end() - 128, Z.bits.end());
+
+        block* h_z0 = new block[2];
+        H.reveal<block>((block*)h_z0, ALICE);
+
+        Z.bits.erase(Z.bits.end() - 128, Z.bits.end());
+        Z.bits.erase(Z.bits.begin(), Z.bits.begin() + u);
+
+        unsigned char* z = new unsigned char[u / 8];
+        Z.reveal<unsigned char>((unsigned char*)z, BOB);
+
+        if (party == ALICE) {
+            size_t v = 128 * ((info_len * 8 + 128 - 1) / 128) - info_len * 8; // 128 * ceil(8*info_len/128) - info_len*8
+
+            io->recv_data(ctxt, m);
+            unsigned char* x = new unsigned char[u / 8 + m + v / 8 + info_len + 16];
+            memcpy(x, aInfo, info_len);
+            memset(x + info_len, 0, v / 8);
+            memcpy(x + info_len + v / 8, ctxt, m);
+            memset(x + info_len + v / 8 + m, 0, u / 8);
+            memcpy(x + info_len + v / 8 + m + u / 8, (unsigned char*)&info_len, 8);
+            memcpy(x + info_len + v / 8 + m + u / 8 + 8, (unsigned char*)&m, 8);
+
+            block* xblk = (block*)&x;
+            block t = ghash(h_z0[0], xblk, (u + 8 * m + v + 8 * info_len) / 128 + 1);
+            t = t ^ h_z0[1];
+            tag = (unsigned char*)&t;
+            io->send_data(tag, 16);
+            delete[] x;
+        } else if (party == BOB) {
+            for (int i = 0; i < m; i++) {
+                ctxt[i] = z[i] ^ msg[i];
+            }
+            io->send_data(ctxt, m);
+            io->recv_data(tag, 16);
+        }
+
+        delete[] h_z0;
+        delete[] z;
+    }
+
+    // inline void dec(NetIO* io, unsigned char* msg, unsigned char* ctxt, size_t m, unsigned char*
+    // tag, unsigned char* iv, unsigned char* aInfo, size_t info_len, int party) {
+    //     if (sizeof(iv) != 12) {
+    //         error("invalid IV length!");
+    //     }
+    //     reverse(iv, iv + sizeof(iv));
+
+    //     Integer J(96, iv, PUBLIC);
+    //     Integer ONE = Integer(32, 1, PUBLIC);
+    //     concat(J, &ONE, 1);
+
+    //     size_t u = 128 * ((m * 8 + 128 - 1) / 128) - m * 8; // 128 * ceil(8m/128) -8m
+    //     size_t v = 128 * ((info_len * 8 + 128 - 1) / 128) - info_len * 8; // 128*
+    //     ceil(8*info_len/128)-info_len*8 size_t ctr_len = (m * 8 + 128 - 1) / 128;
+
+    //     Integer Z;
+    //     gctr(Z, J, 1 + ctr_len);
+
+    //     Integer H = computeH();
+    //     H.bits.insert(H.bits.end(), Z.bits.end() - 128, Z.bits.end());
+
+    //     block* h_z0 = new block[2];
+    //     H.reveal<block>((block*)h_z0, ALICE);
+
+    //     Z.bits.erase(Z.bits.end() - 128, Z.bits.end());
+    //     Z.bits.erase(Z.bits.begin(), Z.bits.begin() + u);
+
+    //     unsigned char* z = new unsigned char[u / 8];
+    //     Z.reveal<unsigned char>((unsigned char*)z, BOB);
+    // }
+};
 #endif
--- test/aesgcm.cpp
+++ test/aesgcm.cpp
@@ -1,9 +1,11 @@
 #include "emp-tool/emp-tool.h"
+#include "backend/backend.h"
 #include "prf/aesgcm.h"
 #include <iostream>
 
 using namespace emp;
 using namespace std;
+
 void mul_test() {
     block H = makeBlock(0x0388DACE60B6A392, 0xF328C2B971B2FE78);
     block C = makeBlock(0x66E94BD4EF8A2C3B, 0x884CFA59CA342B2E);
@@ -15,6 +17,65 @@ void mul_test() {
     cout << "HC2: " << HC2 << endl;
     cout << "expected HC: " << HC << endl;
 
+    block res = zero_block;
+    gfmul(H, C, &res);
+    cout << res << endl;
+    gfmul(C, H, &res);
+    cout << res << endl;
+
+    block a = makeBlock(0x7b5b546573745665, 0x63746f725d53475d);
+    block b = makeBlock(0x4869285368617929, 0x5b477565726f6e5d);
+    gfmul(a, b, &res);
+    cout << res << endl;
+    gfmul(b, a, &res);
+    cout << res << endl;
+    block c = makeBlock(0x6d02da056a66d4cd, 0x035206a56a54d4a9);
+    block d = makeBlock(0x860f0c1fa9ff53ff, 0xc0db81b7b2fd65fb);
+    gfmul(c, d, &res);
+    cout << res << endl;
+    cout << mulBlock(a, b) << endl;
 }
 
-int main() { mul_test(); }
+void ghash_test() {
+    // block h = makeBlock(0xb83b533708bf535d, 0x0aa6e52980d53b78);
+    // block a = zero_block;
+    // block c1 = makeBlock(0x42831ec221777424, 0x4b7221b784d0d49c);
+    // block c2 = makeBlock(0xe3aa212f2c02a4e0, 0x35c17e2329aca12e);
+    // block c3 = makeBlock(0x21d514b25466931c, 0x7d8f6a5aac84aa05);
+    // block c4 = makeBlock(0x1ba30b396a0aac97, 0x3d58e091473f5985);
+    // block x[5] = {a, c1, c2, c3, c4};
+    // //cout << ghash(h, x, 5) << endl;
+
+    block a = makeBlock(0x7b5b546573745665, 0x63746f725d53475d);
+    block b = makeBlock(0x4869285368617929, 0x5b477565726f6e5d);
+    block res = zero_block;
+    gfmul(a, b, &res);
+    cout << res << endl;
+    cout << mulBlock(a, b) << endl;
+}
+
+void aes_test() {
+    Integer a(128, 2, ALICE);
+    Integer b(128, 0, PUBLIC);
+    Bit* c = new Bit[256];
+    memcpy(c, a.bits.data(), 128);
+    memcpy(c + 128, b.bits.data(), 128);
+    Integer o(128, 0, PUBLIC);
+    aes.compute(o.bits.data(), c);
+    cout << o.reveal<string>() << endl;
+    delete[] c;
+}
+
+int main(int argc, char** argv) {
+    int port, party;
+    parse_party_and_port(argv, &party, &port);
+    NetIO* io = new NetIO(party == ALICE ? nullptr : "127.0.0.1", port);
+    setup_backend(io, party);
+
+    aes_test();
+
+    cout << "AND gates: " << dec << CircuitExecution::circ_exec->num_and() << endl;
+    finalize_backend();
+    delete io;
+    //mul_test();
+}
