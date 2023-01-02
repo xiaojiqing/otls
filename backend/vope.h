b7d3a64b37298a24199d73efb0b687f76a890d1e
Xiao Wang <wangxiao1254@gmail.com>
2023-01-02 00:59:01 -0600

    half done

---
+++ backend/vope.h
@@ -0,0 +1,72 @@
+#ifndef PADO_VOPE_H
+#define PADO_VOPE_H
+#include "emp-ot/emp-ot.h"
+#include "backend/bn_utils.h"
+#include "cipher/aesgcm.h"
+#include <iostream>
+template<typename IO>
+class VOPE { public:
+	IO * io;
+	COT<IO>* ot;
+	CCRH ccrh;
+	GaloisFieldPacking pack;
+	VOPE(IO* io, COT<IO>* ot): io(io), ot(ot) {
+	}
+
+	~VOPE() {
+	}
+ 
+	void compute_recv(block * out, int length) {
+		block * M = new block[length*128];
+		bool * bits = new bool[length*128];
+		PRG prg; prg.random_bool(bits, length*128);
+		ot->recv_cot(M, bits, length*128);
+		block diff[2];
+		diff[0] = zero_block;
+		io->recv_data(diff+1, sizeof(block));
+		
+		for(int i = 0; i < length*128; ++i) {
+			M[i] = M[i] ^ diff[bits[i]];
+		}
+
+		for(int i = 0; i < length; ++i)
+			pack.packing(M+i, M+i*128);
+		block * U = M + length;
+		block *t1 = M + 2*length;
+		block *t2 = M + 3*length;
+		for(int i = 0; i < length; ++i)
+			U[i] = bool_to_block(bits+i*128);
+		//M = K + U Delta
+		out[0] = M[0];
+		out[1] = U[0];
+		cout <<"B:"<< out[1][0]<<endl;
+		for(int i = 1; i < length; ++i) {
+			for(int j = 0; j <= i; ++j) {
+				t1[j] = mulBlock(out[j], M[i]);
+				t2[j] = mulBlock(out[j], U[i]);
+			}
+			out[0] = t1[0];
+			for(int j = 1; j <= i; ++j)
+				out[j] = t2[j-1] ^ t1[j];
+			out[i+1] = t2[i];
+		}
+		delete[] M;
+		delete[] bits;
+	}
+	void compute_send(block * out, block h, int length) {
+		block * K = new block[length*128];
+		ot->send_cot(K, length*128);
+		block diff = h ^ ot->Delta;
+		io->send_data(&diff, sizeof(block));
+		io->flush();
+		pack.packing(out, K);
+		block tmp;
+		for(int i = 1; i < length; ++i) {
+			pack.packing(&tmp, K+i*128);
+			*out = mulBlock(*out, tmp);
+		}
+		delete[] K;
+	}
+};
+
+#endif// PADO_VOPE_H
---
+++ test/vope.cpp
@@ -0,0 +1,57 @@
+#include "backend/backend.h"
+#include "backend/vope.h"
+#include <iostream>
+
+using namespace std;
+using namespace emp;
+
+int main(int argc, char** argv) {
+	int port, party;
+	const int num_vope = 6;
+	parse_party_and_port(argv, &party, &port);
+	NetIO* ios[1];
+	for(int i = 0; i < 1; ++i)
+		ios[i] = new NetIO(party == ALICE?nullptr:"127.0.0.1",port+i);
+
+
+	setup_backend(ios[0], party);
+
+	//	FerretCOT<NetIO> * cot = new FerretCOT<NetIO>(party, 1, ios, true, true, ferret_b13);
+
+	auto prot = (PADOParty<NetIO>*)(ProtocolExecution::prot_exec);
+	IKNP<NetIO> * cot = prot->ot;
+	vector<block> out;
+	out.resize(num_vope+1);
+	block h;
+	PRG prg; prg.random_data(&h, sizeof(block));
+	prg.random_data(out.data(), num_vope*sizeof(block));
+
+	auto t1 = clock_start();
+	VOPE<NetIO> vope(ios[0], cot);
+	block B;
+	if(party == ALICE) {
+		vope.compute_send(&B, h, num_vope);
+	} else {
+		vope.compute_recv(out.data(), num_vope);
+	}
+	cout<<"execute" << time_from(t1)<<endl;
+
+	if(party == ALICE) {
+		ios[0]->send_data(&h, sizeof(block));
+		cout <<"A:"<< B[0]<<endl;
+	} else {
+		ios[0]->recv_data(&h, sizeof(block));
+		vector<block> hs;
+		hs.push_back(zero_block);
+		hs.push_back(h);
+		for(int i = 2; i < num_vope; ++i) 
+			hs.push_back(mulBlock(hs.back(), h));
+		block tmp;
+		vector_inn_prdt_sum_red(&tmp, hs.data(), out.data(), num_vope+1);
+	}
+	//	delete cot;
+	finalize_backend();
+	for(int i = 0; i < 1; ++i)
+		delete ios[i];
+
+}
