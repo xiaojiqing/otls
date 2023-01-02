65decbee02d9e435186d4e83cf9e6c7a9dfb99be
Xiang Xie <xiexiangiscas@gmail.com>
2023-01-02 11:26:39 +0800

    update mulBlock

--- cipher/aesgcm.h
+++ cipher/aesgcm.h
@@ -9,70 +9,70 @@ static string circuit_file_location =
   macro_xstr(EMP_CIRCUIT_PATH) + string("bristol_fashion/");
 static BristolFashion aes = BristolFashion((circuit_file_location + "aes_128.txt").c_str());
 
-inline block rsht(block x, size_t i) {
-    uint64_t* data = (uint64_t*)&x;
-    if (i == 0) {
-        return x;
-    } else if (i < 64) {
-        return makeBlock((data[1] >> i), (data[1] << (64 - i)) ^ (data[0] >> i));
-    } else if (i < 128) {
-        return makeBlock(0x00, data[1] >> (i - 64));
-    } else
-        return makeBlock(0x00, 0x00);
-}
+// inline block rsht(block x, size_t i) {
+//     uint64_t* data = (uint64_t*)&x;
+//     if (i == 0) {
+//         return x;
+//     } else if (i < 64) {
+//         return makeBlock((data[1] >> i), (data[1] << (64 - i)) ^ (data[0] >> i));
+//     } else if (i < 128) {
+//         return makeBlock(0x00, data[1] >> (i - 64));
+//     } else
+//         return makeBlock(0x00, 0x00);
+// }
 
-inline block mulBlock(block x, block y) {
-    block Z = zero_block, V = y;
+// inline block mulBlock(block x, block y) {
+//     block Z = zero_block, V = y;
 
-    for (int i = 0; i < 128; i++) {
-        Z = getLSB(rsht(x, 127 - i)) ? Z ^ V : Z;
-        V = getLSB(V) ? rsht(V, 1) ^ R : rsht(V, 1);
-    }
-    return Z;
-}
+//     for (int i = 0; i < 128; i++) {
+//         Z = getLSB(rsht(x, 127 - i)) ? Z ^ V : Z;
+//         V = getLSB(V) ? rsht(V, 1) ^ R : rsht(V, 1);
+//     }
+//     return Z;
+// }
 
-inline block mulBlock2(block a, block b) {
-	__m128i tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8, tmp9;
-	tmp3 = _mm_clmulepi64_si128(a, b, 0x00);
-	tmp4 = _mm_clmulepi64_si128(a, b, 0x10);
-	tmp5 = _mm_clmulepi64_si128(a, b, 0x01);
-	tmp6 = _mm_clmulepi64_si128(a, b, 0x11);
+inline block mulBlock(block a, block b) {
+    __m128i tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8, tmp9;
+    tmp3 = _mm_clmulepi64_si128(a, b, 0x00);
+    tmp4 = _mm_clmulepi64_si128(a, b, 0x10);
+    tmp5 = _mm_clmulepi64_si128(a, b, 0x01);
+    tmp6 = _mm_clmulepi64_si128(a, b, 0x11);
 
-	tmp4 = _mm_xor_si128(tmp4, tmp5); 
-	tmp5 = _mm_slli_si128(tmp4, 8); 
-	tmp4 = _mm_srli_si128(tmp4, 8); 
-	tmp3 = _mm_xor_si128(tmp3, tmp5); 
-	tmp6 = _mm_xor_si128(tmp6, tmp4);
+    tmp4 = _mm_xor_si128(tmp4, tmp5);
+    tmp5 = _mm_slli_si128(tmp4, 8);
+    tmp4 = _mm_srli_si128(tmp4, 8);
+    tmp3 = _mm_xor_si128(tmp3, tmp5);
+    tmp6 = _mm_xor_si128(tmp6, tmp4);
 
-	tmp7 = _mm_srli_epi32(tmp3, 31); 
-	tmp8 = _mm_srli_epi32(tmp6, 31); 
-	tmp3 = _mm_slli_epi32(tmp3, 1); 
-	tmp6 = _mm_slli_epi32(tmp6, 1);
+    tmp7 = _mm_srli_epi32(tmp3, 31);
+    tmp8 = _mm_srli_epi32(tmp6, 31);
+    tmp3 = _mm_slli_epi32(tmp3, 1);
+    tmp6 = _mm_slli_epi32(tmp6, 1);
 
-	tmp9 = _mm_srli_si128(tmp7, 12); 
-	tmp8 = _mm_slli_si128(tmp8, 4); 
-	tmp7 = _mm_slli_si128(tmp7, 4); 
-	tmp3 = _mm_or_si128(tmp3, tmp7); 
-	tmp6 = _mm_or_si128(tmp6, tmp8); 
-	tmp6 = _mm_or_si128(tmp6, tmp9);
+    tmp9 = _mm_srli_si128(tmp7, 12);
+    tmp8 = _mm_slli_si128(tmp8, 4);
+    tmp7 = _mm_slli_si128(tmp7, 4);
+    tmp3 = _mm_or_si128(tmp3, tmp7);
+    tmp6 = _mm_or_si128(tmp6, tmp8);
+    tmp6 = _mm_or_si128(tmp6, tmp9);
 
-	tmp7 = _mm_slli_epi32(tmp3, 31); 
-	tmp8 = _mm_slli_epi32(tmp3, 30); 
-	tmp9 = _mm_slli_epi32(tmp3, 25);
-	tmp7 = _mm_xor_si128(tmp7, tmp8); 
-	tmp7 = _mm_xor_si128(tmp7, tmp9); 
-	tmp8 = _mm_srli_si128(tmp7, 4); 
-	tmp7 = _mm_slli_si128(tmp7, 12);
-	tmp3 = _mm_xor_si128(tmp3, tmp7);
+    tmp7 = _mm_slli_epi32(tmp3, 31);
+    tmp8 = _mm_slli_epi32(tmp3, 30);
+    tmp9 = _mm_slli_epi32(tmp3, 25);
+    tmp7 = _mm_xor_si128(tmp7, tmp8);
+    tmp7 = _mm_xor_si128(tmp7, tmp9);
+    tmp8 = _mm_srli_si128(tmp7, 4);
+    tmp7 = _mm_slli_si128(tmp7, 12);
+    tmp3 = _mm_xor_si128(tmp3, tmp7);
 
-	tmp2 = _mm_srli_epi32(tmp3, 1); 
-	tmp4 = _mm_srli_epi32(tmp3, 2); 
-	tmp5 = _mm_srli_epi32(tmp3, 7); 
-	tmp2 = _mm_xor_si128(tmp2, tmp4); 
-	tmp2 = _mm_xor_si128(tmp2, tmp5); 
-	tmp2 = _mm_xor_si128(tmp2, tmp8); 
-	tmp3 = _mm_xor_si128(tmp3, tmp2);
-	return _mm_xor_si128(tmp6, tmp3);
+    tmp2 = _mm_srli_epi32(tmp3, 1);
+    tmp4 = _mm_srli_epi32(tmp3, 2);
+    tmp5 = _mm_srli_epi32(tmp3, 7);
+    tmp2 = _mm_xor_si128(tmp2, tmp4);
+    tmp2 = _mm_xor_si128(tmp2, tmp5);
+    tmp2 = _mm_xor_si128(tmp2, tmp8);
+    tmp3 = _mm_xor_si128(tmp3, tmp2);
+    return _mm_xor_si128(tmp6, tmp3);
 }
 
 inline block ghash(block h, block* x, size_t m) {
