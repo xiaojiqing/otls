126796d555b25894927dca583c83449518f6daed
Xiao Wang <wangxiao1254@gmail.com>
2023-01-01 21:17:02 -0600

    minor

--- cipher/aesgcm.h
+++ cipher/aesgcm.h
@@ -31,6 +31,50 @@ inline block mulBlock(block x, block y) {
     return Z;
 }
 
+inline block mulBlock2(block a, block b) {
+	__m128i tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8, tmp9;
+	tmp3 = _mm_clmulepi64_si128(a, b, 0x00);
+	tmp4 = _mm_clmulepi64_si128(a, b, 0x10);
+	tmp5 = _mm_clmulepi64_si128(a, b, 0x01);
+	tmp6 = _mm_clmulepi64_si128(a, b, 0x11);
+
+	tmp4 = _mm_xor_si128(tmp4, tmp5); 
+	tmp5 = _mm_slli_si128(tmp4, 8); 
+	tmp4 = _mm_srli_si128(tmp4, 8); 
+	tmp3 = _mm_xor_si128(tmp3, tmp5); 
+	tmp6 = _mm_xor_si128(tmp6, tmp4);
+
+	tmp7 = _mm_srli_epi32(tmp3, 31); 
+	tmp8 = _mm_srli_epi32(tmp6, 31); 
+	tmp3 = _mm_slli_epi32(tmp3, 1); 
+	tmp6 = _mm_slli_epi32(tmp6, 1);
+
+	tmp9 = _mm_srli_si128(tmp7, 12); 
+	tmp8 = _mm_slli_si128(tmp8, 4); 
+	tmp7 = _mm_slli_si128(tmp7, 4); 
+	tmp3 = _mm_or_si128(tmp3, tmp7); 
+	tmp6 = _mm_or_si128(tmp6, tmp8); 
+	tmp6 = _mm_or_si128(tmp6, tmp9);
+
+	tmp7 = _mm_slli_epi32(tmp3, 31); 
+	tmp8 = _mm_slli_epi32(tmp3, 30); 
+	tmp9 = _mm_slli_epi32(tmp3, 25);
+	tmp7 = _mm_xor_si128(tmp7, tmp8); 
+	tmp7 = _mm_xor_si128(tmp7, tmp9); 
+	tmp8 = _mm_srli_si128(tmp7, 4); 
+	tmp7 = _mm_slli_si128(tmp7, 12);
+	tmp3 = _mm_xor_si128(tmp3, tmp7);
+
+	tmp2 = _mm_srli_epi32(tmp3, 1); 
+	tmp4 = _mm_srli_epi32(tmp3, 2); 
+	tmp5 = _mm_srli_epi32(tmp3, 7); 
+	tmp2 = _mm_xor_si128(tmp2, tmp4); 
+	tmp2 = _mm_xor_si128(tmp2, tmp5); 
+	tmp2 = _mm_xor_si128(tmp2, tmp8); 
+	tmp3 = _mm_xor_si128(tmp3, tmp2);
+	return _mm_xor_si128(tmp6, tmp3);
+}
+
 inline block ghash(block h, block* x, size_t m) {
     block y = zero_block;
     for (int i = 0; i < m; i++) {
