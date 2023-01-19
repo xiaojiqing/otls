6de501cb4236a9b98e1121a87d9319b92cc3c185
Xiao Wang <wangxiao1254@gmail.com>
2023-01-19 09:41:26 -0600

    minor

--- test/protocol.cpp
+++ test/protocol.cpp
@@ -8,6 +8,15 @@
 #include "protocol/com_conv.h"
 #include "cipher/aead.h"
 #include "cipher/aead_izk.h"
+#if defined(__linux__)
+	#include <sys/time.h>
+	#include <sys/resource.h>
+#elif defined(__APPLE__)
+	#include <unistd.h>
+	#include <sys/resource.h>
+	#include <mach/mach.h>
+#endif
+
 
 using namespace std;
 using namespace emp;
@@ -312,6 +321,19 @@ int main(int argc, char** argv) {
     com_conv(io1, group, out, party);
 
     EC_GROUP_free(group);
+#if defined(__linux__)
+	struct rusage rusage;
+	if (!getrusage(RUSAGE_SELF, &rusage))
+		std::cout << "[Linux]Peak resident set size: " << (size_t)rusage.ru_maxrss << std::endl;
+	else std::cout << "[Linux]Query RSS failed" << std::endl;
+#elif defined(__APPLE__)
+	struct mach_task_basic_info info;
+	mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+	if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &count) == KERN_SUCCESS)
+		std::cout << "[Mac]Peak resident set size: " << (size_t)info.resident_size_max << std::endl;
+	else std::cout << "[Mac]Query RSS failed" << std::endl;
+#endif
+
 
     delete io1;
     return 0;
