e3674648c6600ff3af1b142492225447cdd41a03
Yilin <ujnss@users.noreply.github.com>
2025-01-22 15:06:02 +0800

    feat: init circuit from memory (default OFF) (#28)

--- cipher/utils.cpp
+++ cipher/utils.cpp
@@ -8,13 +8,22 @@ __thread BristolFormat* aes_ks = nullptr;
 __thread BristolFormat* aes_enc_ks = nullptr;
 #endif
 
+#ifdef LOAD_CIRCUIT_FROM_MEM
+extern std::string aes128_ks_data;
+extern std::string aes128_with_ks_data;
+#else
 static const char* aes_ks_file = "cipher/circuit_files/aes128_ks.txt";
-
 static const char* aes_enc_file = "cipher/circuit_files/aes128_with_ks.txt";
+#endif
 
 void init_files() {
+#ifdef LOAD_CIRCUIT_FROM_MEM
+    aes_ks = new BristolFormat(aes128_ks_data);
+    aes_enc_ks = new BristolFormat(aes128_with_ks_data);
+#else
     aes_ks = new BristolFormat(aes_ks_file);
     aes_enc_ks = new BristolFormat(aes_enc_file);
+#endif
 }
 
 void uninit_files() {
